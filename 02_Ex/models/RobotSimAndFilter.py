124a4019e3a8efeebe9bcebf52b09a85b59c2f0a
Ondin <kafka.ondrej@email.cz>
2023-02-10 12:37:22 +0100

    Added initial Ex_02.

---
+++ 02_Ex/models/RobotSimAndFilter.py
@@ -0,0 +1,23 @@
+
+import random
+import numpy as np
+
+from models import TransitionModel,ObservationModel,StateModel
+
+#
+# Add your Robot Simulator here
+#
+class RobotSim:
+    def __init__(self):
+        print("Hello World")
+        
+#
+# Add your Filtering approach here (or within the Localiser, that is your choice!)
+#
+class HMMFilter:
+    def __init__(self):
+        print("Hello again, World")
+
+        
+        
+        
